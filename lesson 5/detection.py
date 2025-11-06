63dbe7429ca8004e5049dd50c69cd84b4550bedf
denisdutu123 <dutudenis42@gmail.com>
2025-11-06 16:23:56 +0000

    Update detection.py

--- lesson 5/detection.py
+++ lesson 5/detection.py
@@ -13,3 +13,14 @@ cv2.imshow("grey", grey)
 cv2.waitKey(0)
 cv2.imshow("blured", blu)
 cv2.waitKey(0)
+
+dete = cv2.HoughCircles(blu, cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, param2 = 30, minRadius = 1, maxRadius = 45)
+if dete is not None:
+    dete = np.uint16(np.around(dete))
+    for i in dete[0, :]:
+        x, y, r = i[0], i[1], i[2]
+        cv2.circle(image, (x, y), r, (198, 15, 231), 7)
+        cv2.circle(image, (x, y), 1, (198, 15, 231), 7)
+        cv2.imshow("Circle", image)
+        cv2.waitKey(0)
+cv2.destroyAllWindows()
