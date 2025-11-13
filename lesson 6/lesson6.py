15a663369f414026a4d3ffe640aefeab3aa48321
denisdutu123 <dutudenis42@gmail.com>
2025-11-13 16:43:45 +0000

    Update lesson6.py

--- lesson 6/lesson6.py
+++ lesson 6/lesson6.py
@@ -19,6 +19,46 @@ meaheight = meaheight//numimages
 
 print(meawidth, meaheight)
 
+for i in os.listdir("."):
+    if i.endswith(".jpg") or i.endswith(".jpeg") or i.endswith(".avif"):
+        imag  = Image.open(os.path.join(path, i))
+        width, height = imag.size
+        print(width, height)
+        resi = imag.resize((meawidth, meaheight), Image.LANCZOS)
+        resi.save(i, "JPEG", quality = 95)
+        print("image is resized")
+
+#functiong for generating video
+
+def vid():
+    namvid = "Formula 1.avi"
+    os.chdir(r"/Users/denisdutu/Documents/OpenCV/lesson 6/images")
+    filnam = []
+    for i in os.listdir("."):
+        if i.endswith(".jpg") or i.endswith(".jpeg") or i.endswith(".avif"):
+            filnam.append(i)
+    print(filnam)
+    lis = cv2.imread(os.path.join(".", filnam[0]))
+    hei, wid, lay = lis.shape
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    vid = cv2.VideoWriter(namvid, fourcc, 1, (wid, hei))
+    for n in filnam:
+        vid.write(cv2.imread(os.path.join(".", n)))
+    cv2.destroyAllWindows()
+    vid.release()
+    print("video is created", vid)
+    
+
+   
+
+
+vid()
+        
+    
+    
+        
+    
+
 
 
 
