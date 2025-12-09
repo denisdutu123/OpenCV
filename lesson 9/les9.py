df38c9d82224deebe943639361ea854bc9f7c171
denisdutu123 <dutudenis42@gmail.com>
2025-12-09 17:39:36 +0000

    Update les9.py

--- lesson 9/les9.py
+++ lesson 9/les9.py
@@ -11,12 +11,12 @@ score = 0
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 for i in range(total):
-    x = random.randint(0, 500)
-    y = random.randint(0, 500)
+    x = random.randint(0, 1500)
+    y = random.randint(0, 1500)
     size = random.randint(18, 40)
     ball.append({'x':x, 'y':y, 'r':size, 'popped': False})
     
-while True:
+while True:   
     diff = cv2.absdiff(fram1, fram2)
     grscal = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blu = cv2.GaussianBlur(grscal, (5, 5), 0)
@@ -25,8 +25,33 @@ while True:
     contours, _ = cv2.findContours(dilat, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cfram = fram1.copy()
     for i in ball:
-        if not ball["popped"]:
-            cv2.circle(cfram, (ball["x"], ball["y"]), ball["r"], (255, 0, 0), -1)
-    
+        if not i["popped"]:
+            cv2.circle(cfram, (i["x"], i["y"]), i["r"], (255, 0, 0), -1)
+
+    #motion overlap
+    for a in contours:
+        if cv2.contourArea(a) < 1500:
+            continue
+        x, y, wid, hei = cv2.boundingRect(a)
+        #centre of motion
+        centmot = (x + wid // 2, y +hei // 2)
+        cv2.rectangle(cfram, (x, y), (x + wid, y + hei), (89, 233, 12), 7)
+        for q in ball:
+            if not q["popped"]:
+                dist = np.linalg.norm(np.array(centmot) - np.array((q["x"], q["y"])))
+                if dist < q["r"] + 10:
+                    q["popped"]  = True
+                    score +=1
+                    
+    #showing frame with balloons
+    cv2.putText(cfram, "Popped balloons:" + str(score), (100, 1000), font, 1, (123, 56, 234), 4)
+    cv2.imshow("balloon", cfram)
+    fram1 = fram2
+    cap, fram2 = cam.read()
+    key = cv2.waitKey(30)
+    if key == 27:
+        break
 
 
+cam.release()
+cv2.destroyAllWindows()
