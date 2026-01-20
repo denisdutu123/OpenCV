f152c8b913b4ea85afe582887ea69170cea71bae
denisdutu123 <dutudenis42@gmail.com>
2026-01-20 17:20:53 +0000

    Update detec.py

--- colour detecter/detec.py
+++ colour detecter/detec.py
@@ -3,7 +3,7 @@ import numpy as np
 clic = False
 re = gree = blu = x = y = 0
 
-def mous(even, xx, yy):
+def mous(even, xx, yy, flags, param):
     global re, gree, blu, x, y, clic
     if even == cv2.EVENT_LBUTTONDOWN:
         clic = True
@@ -24,3 +24,11 @@ while True :
     if clic == True:
         cv2.rectangle(fra, (20, 20), (650, 91), (blu, gree, re), -1)
         tex = f'R = {re} G={gree} B={blu}'
+        texcol = (255, 255, 255) if re + gree + blu < 600 else (0, 0, 0)
+        cv2.putText(fra, tex, (150, 200), 2, 0.6, texcol, 7, cv2.LINE_AA)
+    cv2.imshow("colour", fra)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
